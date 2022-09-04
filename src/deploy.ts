b8755bd8e4d22248a509d59eb2aefc0fb6b03179
Khiet Tam Nguyen <nktnet999@gmail.com>
2022-09-04 17:24:05 +1000

    Ready for 22T3

---
+++ src/deploy.ts
@@ -0,0 +1,3 @@
+// The URL can be anything you want, as long as it contains your zID exactly once.
+// For example, 'https://anythingyouwant.alwaysdata.net/z5555555-forum'
+export const DEPLOYED_URL = 'https://anythingyouwant.alwaysdata.net/z5555555-forum';
